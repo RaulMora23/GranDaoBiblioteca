3eb858c944bb45f475cafe800c6c380dc81a0277
Andrews Dos Ramos <98641316+andrews0212@users.noreply.github.com>
2025-02-23 20:43:34 +0100

    Finalizacion de PrestamoControlador y UsuarioControlador

--- src/main/java/org/example/grandaobiblioteca/controlador/PrestamoControlador.java
+++ src/main/java/org/example/grandaobiblioteca/controlador/PrestamoControlador.java
@@ -3,6 +3,7 @@ package org.example.grandaobiblioteca.controlador;
 import org.example.grandaobiblioteca.entidad.Prestamo;
 import org.example.grandaobiblioteca.servicio.PrestamoServicio;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -33,6 +34,47 @@ public class PrestamoControlador {
     @DeleteMapping(value = "/{id}", produces = "application/json")
     public boolean deletePrestamo(@PathVariable int id) {
         return servicioPrestamo.deletePrestamo(id);
+    }
+
+    @GetMapping(value = "/XML/{id}", produces = MediaType.APPLICATION_XML_VALUE)
+    public ResponseEntity<Prestamo> findPrestamoXML(@PathVariable int id){
+        return servicioPrestamo.findPrestamo(id);
+    }
+    @GetMapping(value = "/XML", produces = MediaType.APPLICATION_XML_VALUE)
+    public ResponseEntity<List<Prestamo>> findPrestamoALL(){
+        return servicioPrestamo.findALL();
+    }
+    @PostMapping(value = "/XML", produces = MediaType.APPLICATION_XML_VALUE)
+    public ResponseEntity<Prestamo> addPrestamoXML(@RequestBody Prestamo Prestamo) {
+        return servicioPrestamo.addPrestamo(Prestamo) == true ? ResponseEntity.ok(Prestamo) : null;
+    }
+    @PutMapping(value = "/XML", produces = MediaType.APPLICATION_XML_VALUE)
+    public ResponseEntity<Prestamo> updatePrestamoXML(@RequestBody Prestamo Prestamo){
+        return servicioPrestamo.updatePrestamo(Prestamo) == true ? ResponseEntity.ok(Prestamo) : null;
+    }
+    @DeleteMapping(value = "/XML/{id}", produces = MediaType.APPLICATION_XML_VALUE)
+    public ResponseEntity<Prestamo> deletePrestamoXML(@PathVariable int id) {
+        return servicioPrestamo.deletePrestamo(id) == true ? ResponseEntity.ok().build() : null;
+    }
+
+    // Texto
+
+    @GetMapping("/TEXT")
+    public String findALLText(){
+        return servicioPrestamo.findALLText();
+    }
+    @GetMapping(value = "/TEXT/{id}", produces = MediaType.TEXT_PLAIN_VALUE)
+    public String findPrestamoText(@PathVariable int id){
+        return servicioPrestamo.findPrestamo(id).getBody().toString();
+    }
+
+    @PostMapping(value = "/TEXT", produces = MediaType.TEXT_PLAIN_VALUE)
+    public String addPrestamoText(@RequestBody String texto){
+        return servicioPrestamo.addPrestamoText(texto);
+    }
+    @PutMapping(value = "/TEXT/{isbn}", produces = MediaType.TEXT_PLAIN_VALUE)
+    public String updatePrestamoText(@RequestBody String texto, @PathVariable String isbn){
+        return servicioPrestamo.updatePrestamoText(texto);
     }
 
 
--- src/main/java/org/example/grandaobiblioteca/controlador/UsuarioControlador.java
+++ src/main/java/org/example/grandaobiblioteca/controlador/UsuarioControlador.java
@@ -1,7 +1,6 @@
 package org.example.grandaobiblioteca.controlador;
 
 import org.example.grandaobiblioteca.entidad.Usuario;
-import org.example.grandaobiblioteca.entidad.Usuario;
 import org.example.grandaobiblioteca.servicio.UsuarioServicio;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
@@ -9,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+
 @RestController
 @RequestMapping("/usuario")
 public class UsuarioControlador {
@@ -41,4 +41,47 @@ public class UsuarioControlador {
     public ResponseEntity<Usuario> deleteUsuario(@PathVariable int id){
         return servicioUsuario.deleteUsuario(id) == true ? ResponseEntity.ok().build() : null;
     }
+
+
+    @GetMapping(value = "/XML/{id}", produces = MediaType.APPLICATION_XML_VALUE)
+    public ResponseEntity<Usuario> findUsuarioXML(@PathVariable int id){
+        return servicioUsuario.findUsuario(id);
+    }
+    @GetMapping(value = "/XML", produces = MediaType.APPLICATION_XML_VALUE)
+    public ResponseEntity<List<Usuario>> findUsuarioALL(){
+        return servicioUsuario.findALL();
+    }
+    @PostMapping(value = "/XML", produces = MediaType.APPLICATION_XML_VALUE)
+    public ResponseEntity<Usuario> addUsuarioXML(@RequestBody Usuario Usuario) {
+        return servicioUsuario.addUsuario(Usuario) == true ? ResponseEntity.ok(Usuario) : null;
+    }
+    @PutMapping(value = "/XML", produces = MediaType.APPLICATION_XML_VALUE)
+    public ResponseEntity<Usuario> updateUsuarioXML(@RequestBody Usuario Usuario){
+        return servicioUsuario.updateUsuario(Usuario) == true ? ResponseEntity.ok(Usuario) : null;
+    }
+    @DeleteMapping(value = "/XML/{id}", produces = MediaType.APPLICATION_XML_VALUE)
+    public ResponseEntity<Usuario> deleteUsuarioXML(@PathVariable int id) {
+        return servicioUsuario.deleteUsuario(id) == true ? ResponseEntity.ok().build() : null;
+    }
+
+    // Texto
+
+    @GetMapping("/TEXT")
+    public String findALLText(){
+        return servicioUsuario.findALLText();
+    }
+    @GetMapping(value = "/TEXT/{id}", produces = MediaType.TEXT_PLAIN_VALUE)
+    public String findUsuarioText(@PathVariable int id){
+        return servicioUsuario.findUsuario(id).getBody().toString();
+    }
+
+    @PostMapping(value = "/TEXT", produces = MediaType.TEXT_PLAIN_VALUE)
+    public String addUsuarioText(@RequestBody String texto){
+        return servicioUsuario.addUsuarioText(texto);
+    }
+    @PutMapping(value = "/TEXT/{isbn}", produces = MediaType.TEXT_PLAIN_VALUE)
+    public String updateUsuarioText(@RequestBody String texto, @PathVariable String isbn){
+        return servicioUsuario.updateUsuarioText(texto);
+    }
+
 }
