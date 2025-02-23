ea9ae2342a91431b4c9e2dc65a3005e74ceec8ad
Andrews Dos Ramos <98641316+andrews0212@users.noreply.github.com>
2025-02-23 18:07:44 +0100

    elaboracion de prestamo

--- src/main/java/org/example/grandaobiblioteca/controlador/PrestamoControlador.java
+++ src/main/java/org/example/grandaobiblioteca/controlador/PrestamoControlador.java
@@ -1,4 +1,25 @@
 package org.example.grandaobiblioteca.controlador;
 
+import org.example.grandaobiblioteca.entidad.Prestamo;
+import org.example.grandaobiblioteca.servicio.ServicioPrestamo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/Prestamo")
 public class PrestamoControlador {
+
+    @Autowired
+    ServicioPrestamo servicioPrestamo;
+
+    @GetMapping("/JSON")
+    public List<Prestamo> findAllPrestamo(){
+        return servicioPrestamo.obtenerPrestamoes();
+    }
+
+
 }
