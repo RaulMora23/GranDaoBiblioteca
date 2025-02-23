9b5ddc329326d808ee32e7ad3d1e8d4b20fd9252
Raul <raulmoraschz@gmail.com>
2025-02-23 19:17:00 +0100

    Servicio libro,ejemplar

--- src/main/java/org/example/grandaobiblioteca/servicio/EjemplarServicio.java
+++ src/main/java/org/example/grandaobiblioteca/servicio/EjemplarServicio.java
@@ -1,24 +1,14 @@
 package org.example.grandaobiblioteca.servicio;
 
-<<<<<<< Updated upstream
-import org.example.grandaobiblioteca.entidad.Ejemplar;
-
-import org.example.grandaobiblioteca.repositorio.EjemplarRepository;
-
-=======
 
 import org.example.grandaobiblioteca.entidad.Ejemplar;
 import org.example.grandaobiblioteca.entidad.EjemplarMongo;
 import org.example.grandaobiblioteca.repositorio.EjemplarMongoRepo;
 import org.example.grandaobiblioteca.repositorio.EjemplarRepository;
->>>>>>> Stashed changes
 import org.example.grandaobiblioteca.repositorio.LibroRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import java.util.List;
-import java.util.Optional;
-
 import java.util.List;
 import java.util.Optional;
 
@@ -28,14 +18,6 @@ public class EjemplarServicio {
     @Autowired
     private EjemplarRepository ejemplarRepo;
 
-<<<<<<< Updated upstream
-    @Autowired
-    private LibroRepository libroRepository;
-
-    public boolean addEjemplar(Ejemplar Ejemplar){
-        try {
-            ejemplarRepo.save(Ejemplar);
-=======
     private EjemplarMongoRepo mongo;
 
     @Autowired
@@ -44,48 +26,20 @@ public class EjemplarServicio {
         try {
             ejemplarRepo.save(ejemplar);
             mongo.save(new EjemplarMongo(ejemplar));
->>>>>>> Stashed changes
             return true;
         } catch (Exception e) {
             return false;
         }
     }
-<<<<<<< Updated upstream
-    public boolean deleteEjemplar(int id){
-        try{
-            ejemplarRepo.deleteById(id);
-=======
     public boolean deleteEjemplar(Integer id){
         try{
             ejemplarRepo.deleteById(id);
             mongo.deleteById(id);
->>>>>>> Stashed changes
             return true;
         } catch (Exception e){
             return false;
         }
     }
-<<<<<<< Updated upstream
-    public Boolean updateEjemplar(Ejemplar Ejemplar, int id) {
-        try {
-            // Buscar el libro por el ISBN proporcionado
-            Optional<Ejemplar> ejemplarExistente = ejemplarRepo.findById(id);
-
-            if (ejemplarExistente.isPresent()) {
-                // Obtener el libro existente
-                Ejemplar existingEjemplar = ejemplarExistente.get();
-
-                // No modificar el ISBN
-                existingEjemplar.setIsbn(Ejemplar.getIsbn());
-                existingEjemplar.setEstado(Ejemplar.getEstado());
-
-                // Guardar los cambios en el repositorio
-                ejemplarRepo.save(existingEjemplar);
-                return true;
-            } else {
-                // Si no se encuentra el Ejemplar, devolver false
-                System.out.println("Ejemplar con ISBN " + id + " no encontrado.");
-=======
     public Boolean updateEjemplar(Ejemplar ejemplar) {
         try {
             // Buscar el ejemplar por el ISBN proporcionado
@@ -108,62 +62,14 @@ public class EjemplarServicio {
             } else {
                 // Si no se encuentra el ejemplar, devolver false
                 //System.out.println("Ejemplar con ISBN " + isbn + " no encontrado.");
->>>>>>> Stashed changes
                 return false;
             }
         } catch (Exception e) {
             // Registrar la excepción para depuración
-<<<<<<< Updated upstream
-            System.err.println("Error al actualizar el Ejemplar: " + e.getMessage());
-            return false;
-        }
-    }
-
-
-    public ResponseEntity<Ejemplar> findEjemplar(int id){
-        Ejemplar Ejemplar = ejemplarRepo.findById(id).get();
-        return ResponseEntity.ok(Ejemplar);
-
-
-    }
-    public ResponseEntity<List<Ejemplar>> findALL(){
-        List<Ejemplar> Ejemplars = ejemplarRepo.findAll();
-        return ResponseEntity.ok(Ejemplars);
-    }
-
-    public String findALLText(){
-        List<Ejemplar> Ejemplars = this.findALL().getBody();
-        StringBuilder texto = new StringBuilder();
-        for (Ejemplar Ejemplar : Ejemplars) {
-            texto.append(Ejemplar.toString());
-        }
-        return texto.toString();
-    }
-
-    public String findByEjemplarTexto(int id){
-        Ejemplar Ejemplar = this.findEjemplar(id).getBody();
-        return Ejemplar.toString();
-    }
-
-    public String addEjemplarText(String texto){
-        String[] linea = texto.split(",");
-        Ejemplar Ejemplar = new Ejemplar(libroRepository.findById(linea[0]).get(), linea[1]);
-        boolean valor = this.addEjemplar(Ejemplar);
-        return valor == true ? "Ejemplar añadido" : "Error al añadir el Ejemplar";
-    }
-    public String updateEjemplarText(String texto, int id){
-        String[] linea = texto.split(",");
-        Ejemplar Ejemplar = new Ejemplar(libroRepository.findById(linea[0]).get(), linea[1]);
-        boolean valor = this.updateEjemplar(Ejemplar, id);
-        return valor == true ? "Ejemplar actualizado" : "Error al actualizar el Ejemplar";
-    }
-
-=======
             System.err.println("Error al actualizar el ejemplar: " + e.getMessage());
             return false;
         }
     }
->>>>>>> Stashed changes
 
 
     public ResponseEntity<Ejemplar> findEjemplar(Integer id){
